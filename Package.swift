b741061b35585a3c2c06aa9149755efff20d8e26
Petr Brazdil <p.brazdil@gmail.com>
2018-01-04 20:26:50 +0100

    2

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Objc",
+    name: "cppswift",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
